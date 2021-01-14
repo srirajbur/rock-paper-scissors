74ae3f38e38e1907ab0e25d1c641ca5b334f1714
Sriraj Bura <sriraj.bura16@gmail.com>
2021-01-14 16:41:27 -0500

    added logic and style.css

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ function computerPlay(){
     let random = Math.round(Math.random() * 2)
     return arr[random]
 }
+
 let computerScore = 0
 let playerScore = 0
 
 function playRound(playerSelection, computerSelection){
     computerSelection = computerSelection.toLowerCase()
     playerSelection = playerSelection.toLowerCase()
+    console.log(computerSelection)
+    console.log(playerSelection)
     //computer winning
     if(
         (computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scissors' && playerSelection === 'paper')
     ){
         computerScore++
-        return `You Lose! ${computerSelection} beats ${playerSelection}`          //player winning
+        return `${computerSelection} beats ${playerSelection}, Computer Wins!`          //player winning
     } else if(
          (playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')
     ){
@@ -28,29 +31,45 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-let playerSelection;
+// function game(){
+//     for(let i = 0; i < 5; i++){
+//         playRound(playerPlay(), computerPlay())
+//     }
 
-function all123(){
-    playerSelection = prompt('Enter in your input five times')
-}
-const computerSelection = computerPlay();
-
-function game(){
-    for(let i = 0; i < 5; i++){
-        all123()
-        playRound(playerSelection, computerSelection)
-    }
-
-    console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`)
+//     console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`)
     
-    if(computerScore > playerScore){
-        console.log(`Computer Wins!`)
-    } else if(playerScore > computerScore){
-        console.log('Player Wins!')
-    } else{
-        console.log(`Its a tie`)
-    }
-}
-game()
-playerScore = 0
-computerScore = 0
+//     if(computerScore > playerScore){
+//         console.log(`Computer Wins!`)
+//     } else if(playerScore > computerScore){
+//         console.log('Player Wins!')
+//     } else{
+//         console.log(`Its a tie`)
+//     }
+// }
+// game()
+
+// playerScore = 0
+// computerScore = 0
+
+//-------------------------------------
+
+//Selectors
+const btns = document.querySelectorAll('.btn');
+const summary = document.querySelector('.summary');
+const playerResult = document.querySelector('.player');
+const computerResult = document.querySelector('.computer');
+// console.log(choices)
+
+console.log(btns)
+
+//Events
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const playerSelection = btn.id;
+        let result = playRound(playerSelection, computerPlay());
+        playerResult.textContent = `Player: ${playerScore}`;
+        computerResult.textContent = `Computer: ${computerScore}`;
+        summary.textContent = result;
+    })
+})  
+
